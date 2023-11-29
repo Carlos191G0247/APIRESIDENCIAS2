667a5e0ac9f1f8dddcc26c2c8f333a014def736a
carlos gallegos <gallegoscarlos616@gmail.com>
2023-11-29 12:14:47 -0600

    Cambios

--- APIRESIDENCIAS/Controllers/AsginarTareasController.cs
+++ APIRESIDENCIAS/Controllers/AsginarTareasController.cs
@@ -25,11 +25,17 @@ namespace APIRESIDENCIAS.Controllers
             return Ok(entidad);
 
         }
+        [HttpGet("{tareaId}")]
+        public IActionResult GetTarea(int tareaId)
+        {
+            var entidad = repository.Get(tareaId);
+            return Ok(entidad);
+        }
 
         [HttpPost]
         public IActionResult Post(AsignarTareasDTO dto)
         {
-
+             
             Asignaciontareas at = new()
             {
                 Idcoordinador = dto.Idcoordinador,
@@ -43,5 +49,30 @@ namespace APIRESIDENCIAS.Controllers
             return Ok(at.Id);
 
         }
+
+        [HttpPost("PDF")]
+        public IActionResult Post(PdfDTO dto)
+        {
+            var producto = repository.Get(dto.Id);
+
+            if (producto == null)
+            {
+                return BadRequest("No existe el producto especificado");
+            }
+
+            var ruta = webHostEnvironment.WebRootPath + "/tareasasignadas/";
+            System.IO.Directory.CreateDirectory(ruta);
+
+            ruta += dto.Id + ".pdf";
+
+            var file = System.IO.File.Create(ruta);
+
+            byte[] pdf = Convert.FromBase64String(dto.pdfBase64);
+            file.Write(pdf, 0, pdf.Length);
+            file.Close();
+
+            return Ok();
+        }
+
     }
 }
